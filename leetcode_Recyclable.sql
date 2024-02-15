758d32b349a3029f9d4e29311b37869bdc1a581f
Anuj Kumar Chaudhary <117503109+akc2531@users.noreply.github.com>
2024-02-16 01:23:14 +0530

    Create leetcode_Recyclable.sql

---
+++ leetcode_Recyclable.sql
@@ -0,0 +1,3 @@
+/*  https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50 */
+
+Select product_id from products where low_fats="Y" and recyclable ="Y";
