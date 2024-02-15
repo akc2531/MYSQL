ee847964cd04d2df752a8fe080f4e20fcb669979
Anuj Kumar Chaudhary <117503109+akc2531@users.noreply.github.com>
2024-02-16 01:46:24 +0530

    Create BigCountries.sql

---
+++ BigCountries.sql
@@ -0,0 +1,15 @@
+ /*A country is big if:
+
+it has an area of at least three million (i.e., 3000000 km2), or
+it has a population of at least twenty-five million (i.e., 25000000).
+Write a solution to find the name, population, and area of the big countries.
+
+Return the result table in any order.
+
+The result format is in the following example.
+https://leetcode.com/problems/big-countries/description/?envType=study-plan-v2&envId=top-sql-50
+*/
+
+
+# Write your MySQL query statement below
+Select name from Customer where referee_id!=2 OR referee_id is null;
